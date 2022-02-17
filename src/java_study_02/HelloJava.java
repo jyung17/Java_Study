fa71937fb219c33853b77f4bec4d8c1f5c639558
Joo <iotaxixi.gmail.com>
2022-02-17 21:25:33 +0900

    hellojava 파일 수정

--- src/java_study_02/HelloJava.java
+++ src/java_study_02/HelloJava.java
@@ -5,5 +5,6 @@ public class HelloJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello JAVA!!!");
+		System.out.println("Git Commit");
 	}
 }
