1ebd524bbd34624b1378c94cabc18dd1f4f8ed09
jyung17 <iotaxixi@gmail.com>
2022-07-14 09:51:11 +0900

    코딩연습

---
+++ src/main/java/baekjoon/asdfadsfdsaf.java
@@ -0,0 +1,12 @@
+package main.java.baekjoon;
+
+public class asdfadsfdsaf {
+	public static void main(String[] args) {
+		int a = 16;
+		if ((a & 3) == 0) {
+			System.out.println("4의 배수");
+		} else {
+			System.out.println("X");
+		}
+	}
+}
---
+++ src/main/java/ex06/Exam0421.java
@@ -0,0 +1,13 @@
+package main.java.ex06;
+
+public class Exam0421 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1, j = 3, k = 5; i <= 10; i++, j--, k += 2) {
+			System.out.printf("(%d,%d,%d)", i, j, k);
+		}
+		System.out.println();
+	}
+
+}
