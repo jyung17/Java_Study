43a1db3696e59556b259a9252f8bd0554206c61f
Joo <iotaxixi.gmail.com>
2022-07-16 17:33:02 +0900

    0716

--- src/main/java/baekjoon/Main_10951.java
+++ src/main/java/baekjoon/Main_10951.java
@@ -7,10 +7,11 @@ public class Main_10951 {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     Scanner sc = new Scanner(System.in);
-    while (true) {
+    while (sc.hasNextInt()) {
       int a = sc.nextInt();
       int b = sc.nextInt();
       System.out.println(a + b);
     }
+    sc.close();
   }
 }
