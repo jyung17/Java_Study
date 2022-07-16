5417957c364a3e8c71fbcd1502dd0137ac116e24
jyung17 <iotaxixi@gmail.com>
2022-07-16 14:45:17 +0900

    0716

--- src/main/java/baekjoon/Main_10951.java
+++ src/main/java/baekjoon/Main_10951.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class Main_10951 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-
-	}
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Scanner sc = new Scanner(System.in);
+    while (true) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      System.out.println(a + b);
+    }
+  }
 }
