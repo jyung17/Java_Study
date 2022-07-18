0c29991b5e9833c6452efb8993233ead2123898c
jyung17 <iotaxixi@gmail.com>
2022-07-18 09:27:05 +0900

    코딩연습

--- src/main/java/baekjoon/asdfadsfdsaf.java
+++ src/main/java/baekjoon/asdfadsfdsaf.java
@@ -1,12 +1,12 @@
 package main.java.baekjoon;
 
 public class asdfadsfdsaf {
-	public static void main(String[] args) {
-		int a = 16;
-		if ((a & 3) == 0) {
-			System.out.println("4의 배수");
-		} else {
-			System.out.println("X");
-		}
-	}
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("문자열 ").append("연결1").append('\n');
+    sb.append("문자열 ").append("연결2").append('\n');
+    String str = sb.toString();
+    System.out.println(sb);
+    System.out.println(str);
+  }
 }
