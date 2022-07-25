96e8ea83a5b41fc106649e62ea3571b6cdd21668
jyung17 <iotaxixi@gmail.com>
2022-07-25 21:02:57 +0900

    코딩연습

--- board-app/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#----Window----
-#javac -encoding UTF-8 -d app/bin/main/ app/src/main/java/com/bitcamp/board/App.java
-#--------------
-javac -d app/bin/main/ app/src/main/java/com/bitcamp/board/App.java
-echo "compile"
-java -cp app/bin/main/ com.bitcamp.board.App
---
+++ src/main/java/javajungsuk3/source/ch7/test/Circle.java
@@ -0,0 +1,18 @@
+package main.java.javajungsuk3.source.ch7.test;
+
+class Point {
+  int x;
+  int y;
+}
+
+
+public class Circle extends Point {
+  int r;
+}
+
+//public class Circle {
+//  Point c = new Point();
+//  int r;
+//}
+
+
